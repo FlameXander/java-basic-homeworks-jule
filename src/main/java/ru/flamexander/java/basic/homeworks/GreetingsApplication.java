bb26a1b7457a49f96ceea106c227c54b212331b1
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-07-05 21:11:03 +0300

    init

---
+++ src/main/java/ru/flamexander/java/basic/homeworks/GreetingsApplication.java
@@ -0,0 +1,7 @@
+package ru.flamexander.java.basic.homeworks;
+
+public class GreetingsApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
