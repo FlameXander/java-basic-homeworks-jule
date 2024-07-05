7945a4663a91ecef449122693474ac4b446746cc
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-07-05 21:41:15 +0300

    Задача по рисованию квадрата решена

--- src/main/java/ru/flamexander/java/basic/homeworks/GreetingsApplication.java
+++ src/main/java/ru/flamexander/java/basic/homeworks/GreetingsApplication.java
@@ -4,4 +4,10 @@ public class GreetingsApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public static void abc() {
+        System.out.println("---");
+        System.out.println("- -");
+        System.out.println("---");
+    }
 }
