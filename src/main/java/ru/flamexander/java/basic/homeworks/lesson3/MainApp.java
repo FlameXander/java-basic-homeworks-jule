133f4bba0815c75ac88e8044ee2db2cbbb597038
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-07-05 21:36:35 +0300

    правки по замечаниям, исправил что-нибудь

--- src/main/java/ru/flamexander/java/basic/homeworks/lesson3/MainApp.java
+++ src/main/java/ru/flamexander/java/basic/homeworks/lesson3/MainApp.java
@@ -3,15 +3,16 @@ package ru.flamexander.java.basic.homeworks.lesson3;
 public class MainApp {
     // Домашнее задание:
     // - Реализуйте метод printIsNumberPositive, он должен напечатать в консоль
-    // сообщение number + " is positive" если передано не отрицательное число
+    // сообщение number + " - число положительно" если передано не отрицательное число
 
     public static void main(String[] args) {
+        printIsNumberPositive(10);
         printIsNumberPositive(-10);
     }
 
     public static void printIsNumberPositive(int number) {
         if (number >= 0) {
-            System.out.println(number + " is positive");
+            System.out.println(number + " - число положительно");
         }
     }
 }
